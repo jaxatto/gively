82bb72dd1e4118f39b39475878583de2d4a33bc0
Marie Fonkou <mafonkou@visa.com>
2019-09-21 17:21:44 -0500

    mock data for each table

---
+++ seeds/01_associations.js
@@ -0,0 +1,42 @@
+[
+  {
+    name: "Alzheimer’s Texas",
+    address: "3520 Executive Center Drive, Ste. 140, Austin",
+    category_int: 3
+  },
+  {
+    name: "The Arc of the Capital Area",
+    address: "4902 Grover Ave., Austin",
+    category_int: 4
+  },
+  {
+    name: "Breast Cancer Resource Center ",
+    address: "601 E. 15th St., Austin",
+    category_int: 4
+  },
+  {
+    name: "Down Syndrome Association of Central Texas",
+    address: "3710 Cedar St., Box 3, Ste. 287, Austin",
+    category_int: 3
+  },
+  {
+    name: "Helping the Aging, Needy and Disabled Inc.",
+    address: "1640-B E. Second St., Ste. 200, Austin",
+    category_int: 2
+  },
+  {
+    name: "Royal Family Kids Camp",
+    address: "2700 Northland Drive, Austin",
+    category_int: 1
+  },
+  {
+    name: "United Way for Greater Austin",
+    address: "2000 E. MLK Jr. Blvd., Austin",
+    category_int: 4
+  },
+  {
+    name: "YMCA of Austin—Town Lake Branch",
+    address: "1100 W. Cesar Chavez St., Austin",
+    category_int: 1
+  }
+];
