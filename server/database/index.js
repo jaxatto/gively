b75540caf78e0d56e3c715c27e539398a6bff6e0
John Warren <johnpwarren@gmail.com>
2019-09-21 16:22:05 -0500

    database connection

---
+++ server/database/index.js
@@ -0,0 +1,14 @@
+var knex = require("knex")({
+  client: "pg",
+  version: "10.6",
+  connection: {
+    host: "hack.c83tkmoicfii.us-east-1.rds.amazonaws.com",
+    user: "postgres",
+    password: "ribbons13",
+    database: "hack"
+  }
+});
+
+knex("event")
+  .select("id")
+  .then(event => console.log(event));
