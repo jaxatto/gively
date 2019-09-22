9e6b2db750122b60d4a0f90fc2954b2cf35fd503
Alvaro Montoro <alvaromontoro@gmail.com>
2019-09-22 13:19:18 -0500

    Add profile

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import FeedScreen from "./views/feed";
 import EventPageScreen from "./views/eventPage";
+import ProfilePageScreen from "./views/profile";
 
 class App extends React.Component {
   render() {
     const AppNavigator = createStackNavigator(
       {
         Feed: { screen: FeedScreen },
-        EventPage: { screen: EventPageScreen }
+        EventPage: { screen: EventPageScreen },
+        ProfilePage: { screen: ProfilePageScreen }
       },
       {
         initialRouteName: "Feed"
