ebb2403522e7ad42da4f5cc5979c38fb4fcdebdb
Alvaro Montoro <alvaromontoro@gmail.com>
2019-09-22 10:45:50 -0500

    merge conflict + changes

---
+++ components/eventCard.styles.js
@@ -0,0 +1,31 @@
+export const stylesCard = {
+  h3: {
+    fontWeight: "bold",
+    fontSize: 14
+  },
+  col: {
+    display: "flex",
+    justifyContent: "center",
+    flex: "1"
+  },
+  text: {
+    color: "#15AC8C"
+  },
+  star: {
+    fontSize: 20,
+    color: "#15AC8C",
+    margin: 15
+  },
+  arrow: {
+    fontSize: 15,
+    margin: 15,
+    color: "#15AC8C"
+  },
+  signup: {
+    color: "#15AC8C",
+    textAlign: "right",
+    float: "right"
+  }
+};
+
+export default stylesCard;
