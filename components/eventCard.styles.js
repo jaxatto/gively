82ee1cc294c95198def6b2488a0c58900e0c8116
Alvaro Montoro <alvaromontoro@gmail.com>
2019-09-22 11:39:45 -0500

    Styling to match design

--- components/eventCard.styles.js
+++ components/eventCard.styles.js
@@ -1,7 +1,18 @@
 export const stylesCard = {
+  card: {
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+    borderBottomStyle: "solid",
+    marginBottom: 15
+  },
   h3: {
     fontWeight: "bold",
-    fontSize: 14
+    fontSize: 15,
+    marginBottom: 2
+  },
+  description: {
+    fontSize: 15
   },
   col: {
     display: "flex",
@@ -9,22 +20,40 @@ export const stylesCard = {
     flex: "1"
   },
   text: {
-    color: "#15AC8C"
+    color: "#15AC8C",
+    textTransform: "uppercase",
+    fontSize: 14,
+    fontWeight: "bold"
   },
   star: {
-    fontSize: 20,
+    fontSize: 14,
     color: "#15AC8C",
-    margin: 15
+    margin: 15,
+    fontWeight: "bold"
   },
   arrow: {
-    fontSize: 15,
+    fontSize: 14,
     margin: 15,
-    color: "#15AC8C"
+    color: "#15AC8C",
+    fontWeight: "bold"
   },
   signup: {
+    fontSize: 14,
     color: "#15AC8C",
     textAlign: "right",
-    float: "right"
+    float: "right",
+    textTransform: "uppercase",
+    fontWeight: "bold"
+  },
+  dateRow: {
+    color: "#666",
+    marginTop: 10,
+    fontSize: 14
+  },
+  timeRow: {
+    color: "#666",
+    marginBottom: 20,
+    fontSize: 14
   }
 };
 
