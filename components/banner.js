ebc93fb576920618899047ccf4bf4c1c711bf070
Alvaro Montoro <alvaromontoro@gmail.com>
2019-09-22 15:00:20 -0500

    Make banner generic

--- components/banner.js
+++ components/banner.js
@@ -1,24 +1,26 @@
 import React from "react";
-import { H2, Row, Text, Icon, View } from "native-base";
+import { H2, Row, Text, Icon, View, Button } from "native-base";
+import { stylesBanner } from "./banner.styles";
 
-export const Banner = ({ title, body }) => {
+export const Banner = ({ title, body, icon, button }) => {
   return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: "column",
-        backgroundColor: "#15AC8C",
-        padding: 20
-      }}
-    >
+    <View style={stylesBanner.card}>
       <Row>
-        <H2 style={{ color: "white" }}>
-          <Icon name="gift" style={{ color: "white" }} />
+        <H2 style={stylesBanner.colorWhite}>
+          <Icon name={icon} style={stylesBanner.colorWhite} />
           &nbsp;{title}
         </H2>
       </Row>
       <Row>
-        <Text style={{ color: "white" }}>{body}</Text>
+        <Text style={stylesBanner.colorWhite}>{body}</Text>
+      </Row>
+      <Row>
+        <Button rounded transparent style={stylesBanner.button}>
+          <Text>
+            {button}&nbsp;
+            <Icon name="arrow-forward" style={stylesBanner.icon} />
+          </Text>
+        </Button>
       </Row>
     </View>
   );
---
+++ components/banner.styles.js
@@ -0,0 +1,26 @@
+export const stylesBanner = {
+  card: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#15AC8C",
+    padding: 20,
+    paddingTop: 30,
+    paddingBottom: 30
+  },
+  colorWhite: {
+    color: "#fff"
+  },
+  button: {
+    marginTop: 15,
+    color: "#15AC8C",
+    backgroundColor: "white"
+  },
+  icon: {
+    fontSize: 14,
+    margin: 15,
+    color: "#15AC8C",
+    fontWeight: "bold"
+  }
+};
+
+export default stylesBanner;
