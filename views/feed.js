b26847c27b24dfd44e926227dee717a20ba846a3
Marie Fonkou <mafonkou@visa.com>
2019-09-22 08:52:00 -0500

    layout feed page

--- views/feed.js
+++ views/feed.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, Image, StyleSheet } from "react-native";
+import { Col, Row, Grid } from "react-native-easy-grid";
 import {
   Container,
   Header,
   Content,
   Button,
+  List,
+  ListItem,
   Left,
   Right,
   Icon,
@@ -18,23 +21,77 @@ class Feed extends Component {
     return (
       <Container>
         <Container>
-          <Header>
+          <Header transparent>
             <Left>
-              <Title>Hi Sally!</Title>
+              <Title
+                style={{
+                  color: "#0C0778",
+                  padding: 10,
+                  fontSize: 16
+                }}
+              >
+                Hi Sally!
+              </Title>
             </Left>
             <Right>
               <Button transparent>
-                <Icon name="menu" />
+                <Icon
+                  name="menu"
+                  style={{
+                    color: "#0C0778"
+                  }}
+                />
               </Button>
             </Right>
           </Header>
-          <Content>
-            <Text>You have no upcoming events</Text>
-          </Content>
+          <Text
+            style={{
+              fontSize: 18
+            }}
+          >
+            You have no upcoming events
+          </Text>
+          <Grid>
+            <Row>
+              <Col>
+                <Button
+                  rounded
+                  style={{
+                    color: "#15AC8C",
+                    borderColor: "#15AC8C"
+                  }}
+                >
+                  <Text>MY REWARDS</Text>
+                  <Icon name="arrow-forward" />
+                </Button>
+              </Col>
+              <Col></Col>
+            </Row>
+            <Image source={require("../assets/images/pngs/list-empty.png")} />
+            <Grid>
+              <Row>
+                <Text
+                  style={{
+                    color: "#0C0778",
+                    fontSize: 22
+                  }}
+                >
+                  Explore Austin Events
+                </Text>
+              </Row>
+              <Row>
+                <Content padder>
+                  <Row>
+                    <EventCard />
+                  </Row>
+                  <Row>
+                    <EventCard />
+                  </Row>
+                </Content>
+              </Row>
+            </Grid>
+          </Grid>
         </Container>
-        <ScrollView>
-          <EventCard />
-        </ScrollView>
       </Container>
     );
   }
